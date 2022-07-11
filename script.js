2dcbf72cc04fbd0b010f9d65716ccf3810aa5c80
Martijn Wesselius <martijn.wesselius@gmail.com>
2022-07-11 16:23:04 +0200

    Add bookcard creation by JS

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ Library.prototype.addBook = function(newBook) {
     }
 }
 
-Library.prototype.removeBook =function(title) {
+Library.prototype.removeBook = function(title) {
     if (this.isInLibrary(title)) {
         this.books.filter((book) => book.title !== title);
     }
@@ -24,7 +24,7 @@ Library.prototype.getBook = function(title) {
     return this.books.find((book) => book.title === title);
 }
 
-function Book(title, author, pages = undefined, read = false) {
+function Book(title, author, pages, read = false) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -35,7 +35,7 @@ function Book(title, author, pages = undefined, read = false) {
 const theHobbit = new Book("The Hobbit", "JRR Tolkien", 400, false);
 const shoeDog = new Book("Shoe Dog", "Phil Knight", 400, true);
 const sapiens = new Book("Sapiens", "Yuval Noah Harari", 512, false);
-const elonMusk = new Book("Elon Mus", "Ashlee Vance", 448, true);
+const elonMusk = new Book("Elon Musk", "Ashlee Vance", 448, true);
 const randomWalk = new Book("A random walk down Wall Street", "Burton Malkiel", 480, true);
 
 const myLibrary = new Library();
@@ -51,28 +51,123 @@ console.log(myLibrary.books);
 
 // USER INTERFACE
 
+const bookGrid = document.querySelector(".library");
+
+const addBookForm = document.querySelector("#add-book-form");
 const addBookButton = document.querySelector("#add-book-button");
 const overlay = document.querySelector("#overlay");
 const addBookModal = document.querySelector("#add-book-modal");
+const submitButton = document.querySelector("#submit-button");
 const readButton = document.querySelector(".read-button");
 const deleteButton = document.querySelector(".delete-button");
 
 
 
 function openModal() {
-    overlay.classList.add('active')
-    addBookModal.classList.add('active')
+    overlay.classList.add('active');
+    addBookModal.classList.add('active');
 }
 
 function closeModal() {
-    overlay.classList.remove('active')
-    addBookModal.classList.remove('active')
+    overlay.classList.remove('active');
+    addBookModal.classList.remove('active');
 }
+
+function addBookToLibrary() {
+
+    console.log("test")
+
+    const title = document.querySelector("#add-book-form#title").value;
+    const author = document.querySelector("#add-book-form#author").value;
+    const pages = document.querySelector("#add-book-form#pages").value;
+    const isRead =  document.querySelector("#add-book-form#is-read").checked;
+
+    // console.log(title);
+    // console.log(author);
+    // console.log(pages);
+    // console.log(isRead);
+
+    const newBook = new Book(title, author, pages, isRead);
+
+    // Error message when book is already in Library
+
+    myLibrary.addBook(newBook);
+
+    console.log(myLibrary.books);
+    
+
+    closeModal()
+}
+
+function displayBooks() {
+    for (let book in myLibrary.books) {
+        createBookCard(myLibrary.books[book]);
+        console.log(myLibrary.books[book]);
+    }
+}
+
+function resetBooks() {
+
+}
+
+function createBookCard(book) {
+    const bookCard = document.createElement("div");
+    const bookTitle = document.createElement("div");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookRead = document.createElement("p");
+    const readButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = `${book.pages} pages`;
+    deleteButton.textContent = "Delete";
+    if (book.isRead) {
+        bookRead.textContent = "Read? ✅ ";
+        readButton.textContent = "Unread";
+    } else {
+        bookRead.textContent = "Read? ❌ ";
+        readButton.textContent = "Read";
+    }
+
+    bookCard.classList.add("book");
+    bookTitle.classList.add("book-title");
+    bookAuthor.classList.add("book-author");
+    bookPages.classList.add("book-pages");
+    bookRead.classList.add("book-read");
+    readButton.classList.add("read-button");
+    deleteButton.classList.add("delete-button");
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookRead);
+    bookCard.appendChild(readButton);
+    bookCard.appendChild(deleteButton);
+
+    bookGrid.appendChild(bookCard);
+}
+
+function toggleRead(e) {
+    const title = e.target.parentNode.parentNode.firstChild.innerHTML;
+    console.log(title);
+
+
+}
+
+function removeBookFromLibrary() {
+    bookGrid.removeChild()
+}
+
+
+displayBooks();
 
 addBookButton.addEventListener("click", () => openModal());
 overlay.addEventListener("click", () => closeModal());
-// addBookForm.addEventListener("submit", addBook);
-
+submitButton.addEventListener("submit", () => addBookToLibrary());
+readButton.addEventListener("click", (e) => toggleRead(e));
+deleteButton.addEventListener("click", () => removeBookFromLibrary());
 
 
 // LOCAL STORAGE
